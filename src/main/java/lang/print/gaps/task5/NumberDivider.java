7c5a4c03d31bf868e265c9e09e8e45ce298c801d
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-23 00:00:21 +0400

    Changes

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        toBeDivided /= (double) toBeDivided /5;
+        System.out.println(toBeDivided/5);
     }
 }
